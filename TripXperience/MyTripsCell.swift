f31e8b4221c2ae87580e969900a26411fa89f4b1
MiguelAceves <maceves@csumb.edu>
2019-04-10 15:32:40 -0700

    Added the format for MyTrips

---
+++ TripXperience/MyTripsCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTripsCell.swift
+//  TripXperience
+//
+//  Created by Miguel Aceves on 4/10/19.
+//  Copyright © 2019 tripxperience. All rights reserved.
+//
+
+import UIKit
+
+class MyTripsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
