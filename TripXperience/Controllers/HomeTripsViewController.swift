5e7c7eb831e8401ba66028e0b9b526dd5e798142
remberto_nunez <remnunez@csumb.edu>
2019-04-29 14:38:06 -0700

    connected to my trip

---
+++ TripXperience/Controllers/HomeTripsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeTripsViewController.swift
+//  TripXperience
+//
+//  Created by Remberto on 4/29/19.
+//  Copyright © 2019 tripxperience. All rights reserved.
+//
+
+import UIKit
+
+class HomeTripsViewController: UIViewController {
+
+    @IBOutlet weak var HomeTripTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
