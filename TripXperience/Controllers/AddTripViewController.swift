24c16873fcbb6998204edbcfba392f27679d6250
Sergio <sllopis@csumb.edu>
2019-04-29 15:06:12 -0700

    TripCell is set up, working on fetching trips from Firebase

--- TripXperience/Controllers/AddTripViewController.swift
+++ TripXperience/Controllers/AddTripViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import Firebase
 import FirebaseDatabase
-import FirebaseAuth
 
 class AddTripViewController: UIViewController {
     
@@ -22,25 +21,14 @@ class AddTripViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         ref = Database.database().reference()
     }
     
     @IBAction func onAddTripButton(_ sender: Any) {
-        let refereneTrip = self.ref.child("Users").child(userID!).child("Trips").child(titleField.text!)
-        refereneTrip.child("title").setValue(titleField.text)
-        refereneTrip.child("description").setValue(descriptionView.text)
+        let referenceTrip = self.ref.child("Users").child(userID!).child("Trips").child(titleField.text!)
+        referenceTrip.child("title").setValue(titleField.text)
+        referenceTrip.child("description").setValue(descriptionView.text)
         self.dismiss(animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- TripXperience/Controllers/HomeTripsViewController.swift
+++ TripXperience/Controllers/HomeTripsViewController.swift
@@ -7,27 +7,51 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
-class HomeTripsViewController: UIViewController {
+class HomeTripsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+
+    // View Outlet
     @IBOutlet weak var HomeTripTableView: UITableView!
+    
+    // Database Reference
+     var ref: DatabaseReference!
+    
+    // Getting the current user ID to fetch THEIR trips.
+    let userID = Auth.auth().currentUser?.uid
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Database Reference to fetch Trips
+        ref = Database.database().reference()
+     
+        // Connecting the View.
+        HomeTripTableView.delegate = self
+        HomeTripTableView.dataSource = self
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Number of actual cells being returned.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of trips each user has.
+        return 5;
     }
-    */
+    
+    // Declaring reusable cell. (NOT STATIC)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // ID is referencing the Storyboard.
+        let cell = HomeTripTableView.dequeueReusableCell(withIdentifier: "TripCell")
+            as! MyTripsCell
+        
+        
+        
+        return cell
+        
+    }
 
 }
