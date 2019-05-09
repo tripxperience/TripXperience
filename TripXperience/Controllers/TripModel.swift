66d0d733d01624c786ac054096f3c529d1576e51
remberto_nunez <remnunez@csumb.edu>
2019-05-09 15:23:56 -0700

    details

--- TripXperience/Controllers/TripModel.swift
+++ TripXperience/Controllers/TripModel.swift
@@ -13,7 +13,8 @@ class TripModel {
     var title: String?
     var description: String?
     
-    init(title: String?){
+    init(title: String?, description: String?){
         self.title = title
+        self.description = description
     }
 }
